5a040f5e29efd782384aead12b68b0448902f0a8
yaicianis <yaicianis06@gmail.com>
2024-11-03 18:35:57 +0100

    les interfaces

---
+++ app/src/main/java/com/android/todo/MainActivity.java
@@ -0,0 +1,28 @@
+package com.android.todo;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+
+        // Assurez-vous que le bon layout est utilisé
+        setContentView(R.layout.activity_debut);
+
+        // Récupère la vue avec l'id "main" et applique les insets de fenêtre
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
